b8b22f32dd93bdb03f07147cf1ea3dacdeaffeb0
Kamil Kobylarz <xraxus@gmail.com>
2025-03-12 16:39:08 +0100

    add fizzBuzz tests

---
+++ src/FizzBuzz.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+/**
+ * Class FizzBuzz.
+ */
+class FizzBuzz
+{
+    private array $values = [];
+
+    public function fizzBuzz(): void
+    {
+        for ($i = 1; $i <= 100; ++$i) {
+            if ($this->isDivisibleByFifteen($i)) {
+                $this->values[$i] = 'FizzBuzz';
+            } elseif ($this->isDivisibleByThree($i)) {
+                $this->values[$i] = 'Fizz';
+            } elseif ($this->isDivisibleByFive($i)) {
+                $this->values[$i] = 'Buzz';
+            } else {
+                $this->values[$i] = $i;
+            }
+        }
+    }
+
+    public function getValue($index)
+    {
+        return $this->values[$index];
+    }
+
+    private function isDivisibleByThree(int $index): bool
+    {
+        return 0 === $index % 3;
+    }
+
+    private function isDivisibleByFive(int $index): bool
+    {
+        return 0 === $index % 5;
+    }
+
+    private function isDivisibleByFifteen(int $i): bool
+    {
+        return $this->isDivisibleByThree($i) && $this->isDivisibleByFive($i);
+    }
+}
---
+++ tests/FizzBuzzTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Test;
+
+use App\FizzBuzz;
+use PHPUnit\Framework\TestCase;
+
+class FizzBuzzTest extends TestCase
+{
+    public static function dataProviderForFizzBuzz(): \Generator
+    {
+        yield 'Should return one' => [
+            'number' => 1,
+            'expected' => 1,
+        ];
+        yield 'Should return two' => [
+            'number' => 2,
+            'expected' => 2,
+        ];
+        yield 'Should return Fizz for 3' => [
+            'number' => 3,
+            'expected' => 'Fizz',
+        ];
+        yield 'Should return four' => [
+            'number' => 4,
+            'expected' => 4,
+        ];
+        yield 'Should return Buzz for 5' => [
+            'number' => 5,
+            'expected' => 'Buzz',
+        ];
+        yield 'Should return Fizz foe 6' => [
+            'number' => 6,
+            'expected' => 'Fizz',
+        ];
+        yield 'Should return FizzBuzz for 15' => [
+            'number' => 15,
+            'expected' => 'FizzBuzz',
+        ];
+        yield 'Should return Buzz for 100' => [
+            'number' => 100,
+            'expected' => 'Buzz',
+        ];
+        yield 'Should return FizzBuzz for 45' => [
+            'number' => 45,
+            'expected' => 'FizzBuzz',
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForFizzBuzz
+     */
+    public function testFizzBuzz(int $index, $expected)
+    {
+        $fizzBuzz = new FizzBuzz();
+        $fizzBuzz->fizzBuzz();
+        $this->assertEquals($expected, $fizzBuzz->getValue($index));
+    }
+
+    public function testFizzBuzzOne()
+    {
+        $fizzBuzz = new FizzBuzz();
+        $fizzBuzz->fizzBuzz();
+        $this->assertEquals(1, $fizzBuzz->getValue(1));
+    }
+}
